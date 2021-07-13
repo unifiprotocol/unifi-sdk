91e47f8d65278a79f2482ec58759e538f43dfca6
Sergio <sergiazow@gmail.com>
2021-07-13 17:13:17 +0200

    fix: exports

--- packages/staking/lib/Adapters/index.ts
+++ packages/staking/lib/Adapters/index.ts
@@ -1,2 +1,4 @@
 export * from "./IStakingAdapter";
+export * from "./Harmony/HarmonyStakingAdapter";
+export * from "./Tron/TronStakingAdapter";
 export * from "./Factory";
--- packages/staking/lib/index.ts
+++ packages/staking/lib/index.ts
@@ -1,1 +1,2 @@
 export * from "./Adapters";
+export * from "./Errors";
