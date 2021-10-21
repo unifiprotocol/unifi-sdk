eca439dc6a6a356809c21ad3102e49dd8e507ba3
Daniel Blanco Parla <dblancoparla@gmail.com>
2021-10-21 16:35:36 +0200

    fix: native tokens should not throw errors

--- packages/utils/lib/TokenLogoResolvers.ts
+++ packages/utils/lib/TokenLogoResolvers.ts
@@ -1,35 +1,44 @@
 import { toChecksumAddress } from "./utils";
 
+function normalizedToChecksumAddress(hash: string) {
+  try {
+    const checksum = toChecksumAddress(hash);
+    return checksum;
+  } catch (err) {
+    return hash;
+  }
+}
+
 export const TokenLogoResolvers: Record<string, (address: string) => string> = {
   Binance: (hash: string) =>
-    `https://icon-service.unifi.report/icon_bsc?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_bsc?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
   BinanceTestnet: (hash: string) =>
-    `https://icon-service.unifi.report/icon_bsc?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_bsc?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
   Polygon: (hash: string) =>
-    `https://icon-service.unifi.report/icon_matic?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_matic?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
 
   Ethereum: (hash: string) =>
-    `https://icon-service.unifi.report/icon_eth?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_eth?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
   Ropsten: (hash: string) =>
-    `https://icon-service.unifi.report/icon_eth?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_eth?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
 
   Harmony: (hash: string) =>
-    `https://icon-service.unifi.report/icon_one?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_one?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
 
   Iotex: (hash: string) =>
-    `https://icon-service.unifi.report/icon_iotx?token=${toChecksumAddress(
+    `https://icon-service.unifi.report/icon_iotx?token=${normalizedToChecksumAddress(
       hash
     )}&autoResolve=false`,
 };
