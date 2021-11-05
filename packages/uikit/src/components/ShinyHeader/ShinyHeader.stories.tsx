2caaf2966e94ba0f7898e6b5674e6e7dbd61f650
Daniel Blanco Parla <dblancoparla@gmail.com>
2021-11-05 18:11:00 +0100

    @unifiprotocol/uikit v1.7.0

---
+++ packages/uikit/src/components/ShinyHeader/ShinyHeader.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ShinyHeader } from ".";
+
+export default {
+  title: "Components/ShinyHeader",
+};
+
+export const Default = () => (
+  <>
+    <h1>ShinyHeader</h1>
+
+    <ShinyHeader>UI KIT - Shiny Header</ShinyHeader>
+  </>
+);
