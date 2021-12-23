8eb4b492227372086681eddc1cdc16aebef5359d
Carlos Lostao <hastro1990@gmail.com>
2021-12-23 12:31:44 +0100

    feat: batches implemented in web3 multicall adapter

--- packages/core-sdk/lib/Adapters/Multicall/Web3MulticallAdapter.ts
+++ packages/core-sdk/lib/Adapters/Multicall/Web3MulticallAdapter.ts
@@ -16,11 +16,14 @@ type GroupedUseCases = {
   contractAddress: string;
   useCases: Array<{ order: number; useCase: GenericUseCase }>;
 };
+
+type MultiCallParams = { mode?: 'chunks' | 'single', chunkSize?: number }
+
 type OrderedContractUseCaseMap = Record<string, GroupedUseCases>;
 
 export class Web3MulticallAdapter extends MulticallBaseAdapter {
   protected multicall?: Multicall;
-  constructor(adapter: IAdapter) {
+  constructor(adapter: IAdapter, private multiCallParams: MultiCallParams = {mode: 'chunks', chunkSize: 50}) {
     super(adapter);
 
     if (this.isMulticallSupported) {
@@ -35,7 +38,7 @@ export class Web3MulticallAdapter extends MulticallBaseAdapter {
     }
   }
 
-  execute(useCases: GenericUseCase[]): Promise<ExecutionResponse[]> {
+  async execute(useCases: GenericUseCase[]): Promise<ExecutionResponse[]> {
     if (!this.isMulticallSupported) {
       return super.execute(useCases);
     }
@@ -43,9 +46,15 @@ export class Web3MulticallAdapter extends MulticallBaseAdapter {
       useCases.reduce(this.groupUseCasesByContractKeepingOrder.bind(this), {})
     ).map(this.mapGroupedUseCasesToMulticallCtx.bind(this));
 
-    return this.multicall
-      .call(callContexts)
-      .then(this.processMulticallResult.bind(this));
+    const chunks = this.getChunks([...callContexts]);
+    let result : ExecutionResponse<any>[] = []
+    for (let chunk of chunks) {
+      let chunkResult = await this.multicall
+      .call(chunk)
+      .then<ExecutionResponse<any>[]>(this.processMulticallResult.bind(this));
+      this._mergeArray(result, chunkResult);
+    }
+    return result;
   }
 
   private mapGroupedUseCasesToMulticallCtx(
@@ -94,7 +103,7 @@ export class Web3MulticallAdapter extends MulticallBaseAdapter {
     res: ContractCallResults
   ): ExecutionResponse[] {
     return Object.entries(res.results).reduce(
-      (orderedResult, [, contractResult]) => {
+    (orderedResult, [, contractResult]) => {
         return contractResult.callsReturnContext.reduce((obj, cur) => {
           const order = Number(cur.reference);
           const value = this.decodeReturnValues(cur);
@@ -114,5 +123,28 @@ export class Web3MulticallAdapter extends MulticallBaseAdapter {
       },
       []
     );
+  }
+
+  private getChunks(callContexts: ContractCallContext[]) : ContractCallContext[][] {
+    if (this.multiCallParams?.mode === 'chunks') {
+      let chunks : ContractCallContext[][] = [];
+      const CHUNK_SIZE = (this.multiCallParams?.chunkSize || 10);
+      while (callContexts.length > 0) {
+        chunks = chunks.concat([callContexts.splice(0, CHUNK_SIZE)])
+      }
+      return chunks;
+    }
+    return [callContexts];
+  }
+
+  private _mergeArray(result : any[], chunkResult : any[]) {
+    for (let i = 0; i < chunkResult.length; i++) {
+      if (chunkResult[i] !== undefined) {
+        if (result[i] !== undefined) {
+          throw new Error('Two values for the same index')
+        }
+        result[i] = chunkResult[i];
+      }
+    }
   }
 }
