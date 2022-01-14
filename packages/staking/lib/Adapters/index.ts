feee3417386d7dfb10321b47ec7df0776a67b3ae
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-01-14 16:05:40 +0100

    feat: deleted staking pkg

--- packages/staking/lib/Adapters/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./IStakingAdapter";
-export * from "./Harmony/HarmonyStakingAdapter";
-export * from "./Tron/TronStakingAdapter";
-export * from "./Factory";
--- packages/staking/lib/Utils/Sleep.ts
+++
@@ -1,5 +0,0 @@
-export const sleep = (sleepTime: number) => {
-  return new Promise<void>((resolve) => {
-    window.setTimeout(() => resolve(), sleepTime);
-  });
-};
