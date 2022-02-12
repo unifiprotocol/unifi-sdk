95f45368d96906d4af37bb9f4c6aa453a845e8a9
Carlos Lostao <hastro1990@gmail.com>
2022-02-12 11:24:19 +0100

    feat: added ftm config to core-sdk

---
+++ packages/core-sdk/lib/Blockchains/FTM/Config.ts
@@ -0,0 +1,34 @@
+import { FTMToken } from "./NativeToken";
+import { Blockchains, EthChainIds, OfflineConnectors } from "../../Types";
+import { MetamaskConnector } from "../../Connectors/Wallets/MetamaskConnector";
+import { MathWalletConnector } from "../../Connectors/Wallets/MathWalletConnector";
+import { blockchainConfigFactory, web3ConnectorFactory } from "../Utils";
+import { MetamaskCompatibleConnector } from "../../Connectors/Wallets/MetamaskCompatibleConnector";
+
+export const HarmonyConfig = blockchainConfigFactory(
+  {
+    blockchain: Blockchains.FTM,
+    chainId: EthChainIds.FTM,
+    publicRpc: "https://rpc.fantom.network",
+    nativeToken: FTMToken,
+    multicall: {
+      supported: true,
+      address: "0x3BF91Ba76B8B60fD92F588068fAd1e6f50B9d420",
+      tryAggregate: false,
+    },
+    explorer: {
+      baseUrl: "https://ftmscan.com",
+      address: function (address: string) {
+        return `${this.baseUrl}/address/${address}`;
+      },
+      token: function (address: string) {
+        return `${this.baseUrl}/tokens/${address}`;
+      },
+      tx: function (address: string) {
+        return `${this.baseUrl}/tx/${address}`;
+      },
+    },
+  },
+  [MetamaskConnector, MathWalletConnector, MetamaskCompatibleConnector],
+  [web3ConnectorFactory(OfflineConnectors.FTM, "https://rpc.fantom.network")]
+);
