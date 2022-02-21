e871e47fe369aba4f2227388ff0ebd6bc90ac70f
Sergio <sergiazow@gmail.com>
2022-02-21 18:24:30 +0100

    fix: api typings

--- packages/core-sdk/lib/Api.ts
+++ packages/core-sdk/lib/Api.ts
@@ -7,23 +7,23 @@ export const getBlockchainConfig = (
   blockchain: Blockchains
 ): IBlockchainConfig => factory.getBlockchainConfig(blockchain);
 
-export const getBlockchainWalletConnectors =
+export const getBlockchainWalletConnectors: typeof factory.getBlockchainWalletConnectors =
   factory.getBlockchainWalletConnectors.bind(factory);
 
-export const getBlockchainOfflineConnectors =
+export const getBlockchainOfflineConnectors: typeof factory.getBlockchainOfflineConnectors =
   factory.getBlockchainOfflineConnectors.bind(factory);
 
-export const getBlockchainOfflineConnector =
+export const getBlockchainOfflineConnector: typeof factory.getBlockchainOfflineConnector =
   factory.getBlockchainOfflineConnector.bind(factory);
 
-export const getBlockchainConnectors =
+export const getBlockchainConnectors: typeof factory.getBlockchainConnectors =
   factory.getBlockchainConnectors.bind(factory);
 
-export const getBlockchainConnectorByName =
+export const getBlockchainConnectorByName: typeof factory.getBlockchainConnectorByName =
   factory.getBlockchainConnectorByName.bind(factory);
 
-export const getBlockchainConnectorByConfig =
+export const getBlockchainConnectorByConfig: typeof factory.getBlockchainConnectorByConfig =
   factory.getBlockchainConnectorByConfig.bind(factory);
 
-export const getBlockchainExplorer =
+export const getBlockchainExplorer: typeof factory.getBlockchainExplorer =
   factory.getBlockchainExplorer.bind(factory);
