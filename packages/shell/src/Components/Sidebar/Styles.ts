0374c8b4b5bf10dcab90152669c139471c477562
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-02-23 14:29:44 +0100

    fix(Shell): sidebar styles

--- packages/shell/src/Components/Sidebar/Styles.ts
+++ packages/shell/src/Components/Sidebar/Styles.ts
@@ -2,13 +2,14 @@ import { mediaQueries } from "@unifiprotocol/uikit";
 import styled from "styled-components";
 
 export const SidebarWrapper = styled.div<{ isOpen: boolean }>`
-  margin: 1rem;
+  padding: 1rem;
   transition: width 0.25s ease 0s;
   overflow: auto;
   flex-shrink: 0;
 
   ${mediaQueries.xs} {
     width: ${(props) => (props.isOpen ? `15rem` : `0`)};
+    padding: ${(props) => (props.isOpen ? `1rem` : `0`)};
   }
 
   ${mediaQueries.sm} {
