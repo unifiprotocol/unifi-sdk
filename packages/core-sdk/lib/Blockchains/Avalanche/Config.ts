1c90de16ee8654c525f4a5cc9021784fc3cdfba6
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-03-29 15:29:44 +0200

    fix: AVAX logoURI

--- packages/core-sdk/lib/Blockchains/Avalanche/Config.ts
+++ packages/core-sdk/lib/Blockchains/Avalanche/Config.ts
@@ -17,7 +17,7 @@ export const AvalancheConfig = blockchainConfigFactory(
     wrappedToken: AVAXWrappedToken,
     upToken: AVAXUpToken,
     logoURI:
-      "https://proxy.unifiprotocol.com/ipfs/QmX4WzftcjqPpw1GUgVGDDLTk1aUwtFZtSzBtv6Zt1RTBZ",
+      "https://proxy.unifiprotocol.com/ipfs/QmWPVF97Jrf4QZZwsfgbViZRYFCqg8jyU3gH72QwLPTF64",
     multicall: {
       supported: true,
       address: "0xa00FB557AA68d2e98A830642DBbFA534E8512E5f",
