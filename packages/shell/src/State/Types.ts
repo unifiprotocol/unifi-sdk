40c3528717d49e0d9438b2760c9ca1ef05003317
Sergio <sergiazow@gmail.com>
2022-05-10 13:09:00 +0200

    doc: add some docs to understand the root state and its partial reducers

--- packages/shell/src/State/Types.ts
+++ packages/shell/src/State/Types.ts
@@ -1,6 +1,20 @@
 import { ShellAction } from "./ShellContext";
 import { ShellState } from "./ShellState";
 
+/**
+ * We have a single state on ShellState with properties on the root
+ * representing different features of the shell like the Adapter and the Balances.
+ *
+ * A partial reducer will be able to:
+ *  - modify only the state in one of those properties in the root.
+ *  - read the whole state but not modify it
+ * @constructor
+ * @param {Type} state - The partial state which is responsible of
+ * @param {ShellAction} action - The action that triggered the reducer
+ * @param {ShellState} rootState - The root state
+ * @returns {Type} state - The partial state reduced
+ *
+ */
 export type ShellPartialReducer<Type> = (
   state: Type,
   action: ShellAction,
